00bef50f2a0b8851f0346797ac7ede8164b993e9
nerea_prog <narvaeznerea18@gmail.com>
2025-01-28 15:24:39 +0100

    Haciendo

---
+++ bloc2_NereaNarvaez/connect.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+def connection_db():
+    conn = psycopg2.connect(
+        database="the_bear",
+        password="admin",
+        user="admin",
+        host="localhost",
+        port="5432"
+    )
+
+    return conn
+
+connection = connection_db()
+print(connection)
+
+connection.close()
+print(connection)
